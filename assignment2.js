71261d31fd8e8d8b32f655df1da0ce4a950164ba
Ayush Shukla <shukla.ayush1729@gmail.com>
2019-01-30 02:08:29 +0530

    Initial layout

---
+++ assignment2.js
@@ -0,0 +1,23 @@
+const URL = "https://in.bookmyshow.com/serv/getData?cmd=GETTRAILERS&mtype=cs";
+let movieData;
+let movieList = "";
+$(document).ready(function () {
+    $.get(URL, function (data, status) {
+        movieData = JSON.parse(data)[1];
+        // console.log(movieData);
+        for (let property in movieData) {
+            if (movieData.hasOwnProperty(property)) {
+                console.log(movieData[property]);
+                movieList += `
+                <div class="movie-box">
+                <div class="movie-image">
+                <img src="https://in.bmscdn.com/events/moviecard/${movieData[property]["EventCode"]}.jpg" alt="${movieData[property]["EventTitle"]}" height="200" width="160"></img>
+                </div>
+                <div class="movie-name">${movieData[property]["EventName"]}</div>
+                </div>`
+            }
+        }
+        // console.log(movieList);
+        $('#bms-list-body').append(movieList);
+    });
+});
