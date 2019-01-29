37191c0d2e78e3d37a1c422fdeb03c78c84dd2e9
Ayush Shukla <shukla.ayush1729@gmail.com>
2019-01-30 02:49:27 +0530

    Added Play Icon

--- assignment2.js
+++ assignment2.js
@@ -7,11 +7,12 @@ $(document).ready(function () {
         // console.log(movieData);
         for (let property in movieData) {
             if (movieData.hasOwnProperty(property)) {
-                console.log(movieData[property]);
+                // console.log(movieData[property]);
                 movieList += `
                 <div class="movie-box">
                 <div class="movie-image">
                 <img src="https://in.bmscdn.com/events/moviecard/${movieData[property]["EventCode"]}.jpg" alt="${movieData[property]["EventTitle"]}" height="200" width="160"></img>
+                <div class="centered"><i class="fa fa-play-circle-o"></i></div>
                 </div>
                 <div class="movie-name">${movieData[property]["EventName"]}</div>
                 </div>`
